f0b0d769806bc6a86eac782114e89be32bc160bf
Gonzalo J Perez <gonzaloperezbarrios@hotmail.com>
2016-04-06 16:18:17 -0500

    Estable Formulario Genrico: Texto Numero Text Area Fecha Check list Check list - Buscador Check single Radio Button

--- src/AppBundle/Controller/PruebaController.php
+++ src/AppBundle/Controller/PruebaController.php
@@ -8,9 +8,12 @@ use Symfony\Component\HttpFoundation\Request;
 
 use AppBundle\Entity\Prueba;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 
@@ -91,15 +94,68 @@ class PruebaController extends Controller
  
         $form = $this->createFormBuilder()
         
-            ->add('Texto', TextType::class,array('label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
-                                                                           'attr' => array('class' => 'form-control col-md-7 col-xs-12')))
-            ->add('utilidad', IntegerType::class,array('label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
-                                                                           'attr' => array('class' => 'form-control col-md-7 col-xs-12')))
-            ->add('plazo', IntegerType::class,array('label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
-                                                                           'attr' => array('class' => 'form-control col-md-7 col-xs-12')))
-            ->add('Fecha', TextType::class,array('label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
-                                                                           'attr' => array('class' => 'date-picker date-picker-control form-control col-md-7 col-xs-12')))
-            //->add('save', SubmitType::class, array('label' => 'Guardar','attr' => array('class' => 'btn btn-success')))
+            ->add('Texto', TextType::class,array('label' => 'Texto *', 
+                                                'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                'attr' => array('class' => 'col-md-7 col-xs-12')))
+            ->add('Numero', IntegerType::class,array('label' => 'Numero *', 
+                                                    'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                    'attr' => array('class' => 'col-md-7 col-xs-12')))
+           
+            ->add('TextoArea', TextareaType::class,array('label' => 'Text Area *', 
+                                                         'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                         'attr' => array('class' => 'col-md-7 col-xs-12')))
+            
+            ->add('Fecha', TextType::class,array('label' => 'Fecha *',
+                                                 'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                 'attr' => array('class' => 'date-picker-control col-md-7 col-xs-12')))
+            ->add('isAttending', ChoiceType::class, array(
+                                                    'choices'  => array(
+                                                                        'Maybe' => null,
+                                                                        'Yes' => true,
+                                                                        'No' => false,
+                                                                        ),    
+                                                 'label' => 'Check List *',                                                
+                                                 'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                 'attr' => array('class' => 'col-md-7 col-xs-12')
+                                                 )) 
+             ->add('isAttending33', ChoiceType::class, array(
+                                                    'choices'  => array(
+                                                                        'Maybe' => null,
+                                                                        'Yes' => true,
+                                                                        'No' => false,
+                                                                        ),    
+                                                 'label' => 'Check List *',                                                
+                                                 'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                 'attr' => array('class' => 'select2_single col-md-7 col-xs-12')
+                                                 ))                                                                                    
+            ->add('public', CheckboxType::class, array(                                                
+                                                'required' => false,
+                                                 'label' => 'Single Check *',                                                 
+                                                 'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                 'attr' => array('class' => 'flat')
+                                                 )) 
+                                                 
+         
+            ->add('attending2', ChoiceType::class, array(
+                                                    'choices' => array(
+                                                        'Yes' => true,
+                                                        'No' => false,
+                                                        'Maybe' => null,
+                                                    ),
+                                                    'choices_as_values' => true,
+                                                    'expanded' => true,
+                                                    'multiple' => false,
+                                                    'label_attr' => array('class' => 'control-label col-md-3 col-sm-3 col-xs-12'),
+                                                    'choice_attr' => function($val, $key, $index) {
+                                                        // adds a class like attending_yes, attending_no, etc
+                                                        return ['class' => 'flat'];
+                                                    },
+                                                ))                 
+                             
+                                                              
+                
+                                                 
+                    // ->add('cancelar', ResetType::class, array('label' => 'Cancelar','attr' => array('class' => 'btn btn-success')))
             
             ->getForm();  
          $form->handleRequest($request);         
